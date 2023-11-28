2bf563594d3bf94801a761460a2d7b7dd211626f
iamwho77 <152010693+iamwho77@users.noreply.github.com>
2023-11-28 07:34:18 +0000

    done

---
+++ jscript/index.js
@@ -0,0 +1,24 @@
+//             node jscript/index.js
+// function c(a, ...b) { 
+//     return [a,...b]
+//      ; 
+
+// }
+// let a = [ 1,2,3,4]
+//  let b = [ 5,6,7,8]
+// console.log(c(a,b))
+// let user =  { 
+//      username  : " baf"  ,
+//      num : 44 , 
+//  h : function(){
+//  console.log(`name is ${this.username} and num is ${this.num}`);} 
+//  } 
+// user.h() ;
+ 
+// ( function k () { 
+//       console.log("gfbgf");   // Immediatelly Invoked Function Expressions
+
+//  })() 
+
+// 
+ 
